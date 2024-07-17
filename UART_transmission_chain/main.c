8d5376cf0114332530667edd511f40abb289ae34
Paul Hain <paulhain@icloud.com>
2024-07-17 12:16:44 +0200

    Working linked list

--- UART_transmission_chain/main.c
+++ UART_transmission_chain/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_nucleo.h"
 
@@ -15,22 +16,61 @@ uint8_t rcv2[BUF_SZ];
 uint8_t rcv3[BUF_SZ];
 const unsigned char NEWLINE = '\n';
 
+// Linked List
+struct LinkedList {
+    uint8_t value;
+    struct LinkedList* next;
+    struct LinkedList* prev;
+};
+struct LinkedList* data_start = NULL;
+struct LinkedList* data_end = NULL;
+unsigned char transfer_finished = 'n';
+
+void insertAtEnd(uint8_t val) {
+   struct LinkedList *newItem = (struct LinkedList*) malloc(sizeof(struct LinkedList));
+   newItem->value = val;
+   newItem->prev = data_end;
+   newItem->next = NULL;
+
+   if (data_end != NULL) {
+        data_end->next = newItem;
+    } else {
+        data_start = newItem;
+    }
+    data_end = newItem;
+}
+
+void reTransferData() {
+    struct LinkedList *currentItem = data_start;
+    while (currentItem != NULL) {
+        HAL_UART_Transmit(&huart2, &currentItem->value, 1, HAL_MAX_DELAY);
+        currentItem = currentItem->next;
+    }
+    transfer_finished = 'n';
+}
+// Linked List End
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == USART2) {
         HAL_UART_Transmit(&huart3, rcv2, 1, HAL_MAX_DELAY);
 
-        if (rcv2[0] == '\r') {
-                HAL_UART_Transmit(&huart3, &NEWLINE, 1, HAL_MAX_DELAY);
-        }
-        
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14); // Toggle LED to signal character reception
         HAL_UART_Receive_IT(&huart2, rcv2, 1); // Restart the interrupt
     } else if (huart->Instance == USART3) {
-        HAL_UART_Transmit(&huart2, rcv3, 1, HAL_MAX_DELAY);
+        // HAL_UART_Transmit(&huart2, rcv3, 1, HAL_MAX_DELAY);
 
-        if (rcv3[0] == '\r') {
-                HAL_UART_Transmit(&huart2, &NEWLINE, 1, HAL_MAX_DELAY);
+        // if (rcv3[0] == '\r') {
+        //     HAL_UART_Transmit(&huart2, &NEWLINE, 1, HAL_MAX_DELAY);
+        // }
+
+        if (rcv3[0] == 35) {
+            transfer_finished = 'y';
+            HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0); // Toggle LED to signal character reception
+            HAL_UART_Receive_IT(&huart3, rcv3, 1); // Restart the interrupt
+            return;
         }
+
+        insertAtEnd(rcv3[0]);
 
         HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0); // Toggle LED to signal character reception
         HAL_UART_Receive_IT(&huart3, rcv3, 1); // Restart the interrupt
@@ -44,11 +84,18 @@ int main(void) {
     USART2_UART_Init();
     USART3_UART_Init();
 
+    data_start = NULL;
+    data_end = NULL;
+
     HAL_UART_Receive_IT(&huart2, rcv2, 1); // Start UART receive interrupt
     HAL_UART_Receive_IT(&huart3, rcv3, 1); // Start UART receive interrupt
 
     while (1) {
-        // Main loop does nothing, all work is done in interrupts
+        if (transfer_finished == 'y') {
+            reTransferData();
+        } else {
+            HAL_Delay(1); //Keep the CPU active
+        }
     }
 }
 
